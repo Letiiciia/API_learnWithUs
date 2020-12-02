6770039a9cd665b7dbe263bc0ada8ce41df5674e
letiiciia <leticialimaferreira2@gmail.com>
2020-12-02 00:29:35 -0300

    Connection repository

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
-//app.use('/exercise');
+//app.use('/exercises')/
+
 
 module.exports = app;
---
+++ src/model/learnWithUs_repository.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const DB_URL = 'mongodb://localhost:27017/reprograma';
+
+const connect = () =>{
+    mongoose.connect(DB_URL, {useNewUrlParser: true});
+    const connection = mongoose.connection;
+    connection.on('error', () => console.log('Error to connect with mongodb'));
+    connection.once('open', () => console.log('We are connect with mongodb'));
+}
+
+module.exports = {connect}
