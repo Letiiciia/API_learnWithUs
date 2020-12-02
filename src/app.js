2c4e755d7a3578dfc1d2ac41aaccfcfbb8044e23
letiiciia <leticialimaferreira2@gmail.com>
2020-12-02 00:33:09 -0300

    app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
+const database = require('./model/learnWithUs_repository');
+const exercisesRoute = require('./routes/learnWithUs_routes');
+
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-//app.use('/exercises')/
-
+database.connect();
+app.use(cors());
+app.use(express.json());
+app.use('/', exercisesRoute);
 
 module.exports = app;
