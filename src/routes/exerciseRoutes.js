4ec72a0d944b3fb87ffaf2eca6692bab9c460d4d
letiiciia <leticialimaferreira2@gmail.com>
2020-12-11 14:28:20 -0300

    fixing getById method

--- src/routes/exerciseRoutes.js
+++ src/routes/exerciseRoutes.js
@@ -5,7 +5,7 @@ const exerciseController = require('../controller/exerciseControllers');
 router.get('/all', exerciseController.getAll);
 router.post('/new', exerciseController.addExercise);
 router.get('/:id', exerciseController.getById);
-router.get('/theme', exerciseController.getByTheme); //query
+router.get('/', exerciseController.getByTheme); //query
 router.patch('/update/:id', exerciseController.updateById);
 router.delete('/delete/:id', exerciseController.deleteById);
 router.delete('/delete', exerciseController.deleteByAuthor);
